b410dc5954d410620831ba1cb7fa9db759edc145
Zamzani <mseptian354@gmail.com>
2024-08-06 16:24:20 +0700

    NGC 1 uhuy

---
+++ Main.py
@@ -0,0 +1,4 @@
+umur_abim = int(input("Masukan Umur nya si abim dalam (tahun) :  ")) #convert 'number' to 'integer number'
+umur_ciput = int(input("Masukan Umurnya ciput dalam (tahun) : "))
+
+print(umur_abim == umur_ciput)
